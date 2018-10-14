be2b27b20ce1592bc240ed92a121214f96239439
a82448186 <464969626@qq.com>
2018-10-14 15:27:53 +0800

    '第一次'

---
+++ examples/routers/cascader.vue
@@ -0,0 +1,66 @@
+<template>
+    <div style="margin: 100px;width: 300px;">
+        <Cascader v-model="value" :data="data" change-on-select></Cascader>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value: ['beijing', 'gugong'],
+                data: []
+            }
+        },
+        mounted () {
+            setTimeout(() => {
+                this.data = [{
+                    value: 'beijing',
+                    label: '北京',
+                    children: [
+                        {
+                            value: 'gugong',
+                            label: '故宫'
+                        },
+                        {
+                            value: 'tiantan',
+                            label: '天坛'
+                        },
+                        {
+                            value: 'wangfujing',
+                            label: '王府井'
+                        }
+                    ]
+                }, {
+                    value: 'jiangsu',
+                    label: '江苏',
+                    children: [
+                        {
+                            value: 'nanjing',
+                            label: '南京',
+                            children: [
+                                {
+                                    value: 'fuzimiao',
+                                    label: '夫子庙',
+                                }
+                            ]
+                        },
+                        {
+                            value: 'suzhou',
+                            label: '苏州',
+                            children: [
+                                {
+                                    value: 'zhuozhengyuan',
+                                    label: '拙政园',
+                                },
+                                {
+                                    value: 'shizilin',
+                                    label: '狮子林',
+                                }
+                            ]
+                        }
+                    ],
+                }];
+            }, 100)
+        }
+    }
+</script>
---
+++ examples/routers/input.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{ value1 }}
+        <Input v-model="value1" clearable icon="ios-clock-outline" size="large" placeholder="large size"></Input>
+        <br>
+        <Input v-model="value2" clearable placeholder="default size"></Input>
+        <br>
+        <Input v-model="value3" clearable size="small" placeholder="small size"></Input>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value1: '',
+                value2: '',
+                value3: ''
+            }
+        }
+    }
+</script>
